5d2cceea666ff001d7a61be454145c2347dfa160
Räbinä Henry <rabinah1@niksula.hut.fi>
2017-07-17 17:03:44 +0300

    Changed language to C++

--- Main.cpp
+++ Main.cpp
@@ -7,12 +7,12 @@ int main() {
   int quit = 0;
   SDL_Window *window;
   SDL_Renderer *renderer;
-  SDL_Init(SDL_INIT_VIDEO);
-  window = SDL_CreateWindow("An SDL2 window",
-			    10, // X-coordinate of the upper left corner of the window
-			    10, // Y-coordinate of the upper left corner of the window
-			    640, // Size of the window in x-direction
-			    480, // Size of the window in y-direction
+  SDL_Init(SDL_INIT_EVERYTHING);
+  window = SDL_CreateWindow("Space Invaders",
+			    100, // X-coordinate of the upper left corner of the window
+			    50, // Y-coordinate of the upper left corner of the window
+			    1000, // Size of the window in x-direction
+			    700, // Size of the window in y-direction
 			    SDL_WINDOW_OPENGL // In here we can add different flags. Syntax for multiple flags is: flag1 | flag2 | flag3...
 			    );
   if (window == NULL) { // If the creation of the window was not succesfull
@@ -20,8 +20,15 @@ int main() {
     return 1;
   }
   renderer = SDL_CreateRenderer(window, -1, 0); // This function creates a 2D rendering context for the window
-  SDL_SetRenderDrawColor(renderer, 0,0,0,255); // The integer parameters are r,g,b,a where a is the alpha-value
+  SDL_SetRenderDrawColor(renderer, 255,255,255,255); // The integer parameters are r,g,b,a where a is the alpha-value
   SDL_RenderClear(renderer); // This function is used to clear the current rendering target with the drawing color
+  SDL_Rect r;
+  r.x = 10;
+  r.y = 640;
+  r.w = 50;
+  r.h = 50;
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderFillRect(renderer, &r);
   SDL_RenderPresent(renderer); // This function is used to update the screen with any rendering performance since the previous call
   while (quit == 0) {
     if (SDL_PollEvent(&event))
