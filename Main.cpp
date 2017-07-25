614f33ee48424325d6744d1cf7c86f6a125eafed
Räbinä Henry <rabinah1@niksula.hut.fi>
2017-07-25 20:55:24 +0300

    Added an image for the spaceship of the player.

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,17 @@
 #include "SDL.h"
+#include "SDL_image.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include "Player.h"
 #include "Enemy.h"
 
+#define IMG_PATH "Space_ship.png"
+
 int main() {
   int speed = 0;
   int s = 0;
+  int w, h;
   Player player_1; // Create an object "player_1" of class "Player".
   player_1.Set_lives(15); // Set the lives of the player to be 15.
   player_1.Set_score(100); // Set the score of the player to be 100.
@@ -20,6 +24,7 @@ int main() {
   int quit = 0;
   SDL_Window *window;
   SDL_Renderer *renderer;
+  SDL_Texture *img = NULL;
   
   SDL_Init(SDL_INIT_EVERYTHING); // Initialize the SDL-window.
   window = SDL_CreateWindow("Space Invaders",
@@ -34,16 +39,19 @@ int main() {
     return 1;
   }
   
-  renderer = SDL_CreateRenderer(window, -1, 0); // This function creates a 2D rendering context for the window
-  SDL_SetRenderDrawColor(renderer, 255,255,255,255); // The integer parameters are r,g,b,a where a is the alpha-value
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); // This function creates a 2D rendering context for the window
+  img = IMG_LoadTexture(renderer, IMG_PATH);
+  SDL_QueryTexture(img, NULL, NULL, &w, &h);
+  SDL_SetRenderDrawColor(renderer, 0,0,0,255); // The integer parameters are r,g,b,a where a is the alpha-value
   SDL_RenderClear(renderer); // This function is used to clear the current rendering target with the drawing color
   SDL_Rect r; // A structure that defines a rectangle
   r.x = 10; // The x-coordinate of the rectangle's upper left corner
-  r.y = 650; // The y-coordinate of the rectangle's upper left corner
-  r.w = 30; // The width of the rectangle
-  r.h = 30; // The height of the rectangle
+  r.y = 655; // The y-coordinate of the rectangle's upper left corner
+  r.w = w; // The width of the rectangle
+  r.h = h; // The height of the rectangle
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderFillRect(renderer, &r);
+  SDL_RenderCopy(renderer, img, NULL, &r);
+  //SDL_RenderFillRect(renderer, &r);
   SDL_RenderPresent(renderer); // This function is used to update the screen with any rendering performance since the previous call
   
   while (quit == 0) {
@@ -57,19 +65,21 @@ int main() {
 	   
 	 case SDL_KEYDOWN: // If some keyboard key is pressed down
 	   if (event.key.keysym.sym == SDLK_RIGHT) { // If the pressed key is the right arrow.
-	     SDL_SetRenderDrawColor(renderer, 255,255,255,255); // Set the render draw color to black.
+	     SDL_SetRenderDrawColor(renderer, 0,0,0,255); // Set the render draw color to black.
 	     SDL_RenderClear(renderer);
 	     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	     r.x = r.x + s;
-	     SDL_RenderFillRect(renderer, &r);
+	     //SDL_RenderFillRect(renderer, &r);
+	     SDL_RenderCopy(renderer, img, NULL, &r);
 	     SDL_RenderPresent(renderer);
 	  }
 	   else if (event.key.keysym.sym == SDLK_LEFT) { // If the pressed key is the left arrow.
-	     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	     SDL_RenderClear(renderer);
 	     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	     r.x = r.x - s;
-	     SDL_RenderFillRect(renderer, &r);
+	     //SDL_RenderFillRect(renderer, &r);
+	     SDL_RenderCopy(renderer, img, NULL, &r);
 	     SDL_RenderPresent(renderer);
 	   }
 	   else if (event.key.keysym.sym == SDLK_SPACE) { // If the pressed key is the space button.
@@ -78,6 +88,8 @@ int main() {
 	 }
       }
   }
+  SDL_DestroyTexture(img);
+  SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window); // This destroys the window
   SDL_Quit();
   return 0;
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
 main: Main.cpp Player.cpp
-	g++ -I /usr/include/SDL2 -o main Main.cpp Player.cpp -lSDL2
+	g++ -I /usr/include/SDL2 -o main Main.cpp Player.cpp -lSDL2 -lSDL2_image
 	./main
